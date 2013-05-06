c1f08f709e5b6c71ae9ed9de649f4f05819e91ea
minhoryang <minhoryang@gmail.com>
2013-05-06 16:33:47 +0900

    [PRJ4] Upload New Testcase File!

---
+++ src/2-9.asm
@@ -0,0 +1,53 @@
+  5 COPY   START  0
+ 10 FIRST  STL    RETADR
+ 13        LDB    #LENGTH
+ 14        BASE   LENGTH
+ 15 CLOOP  +JSUB  RDREC
+ 20        LDA    LENGTH
+ 25        COMP   #0
+ 30        JEQ    ENDFIL
+ 35        +JSUB  WRREC
+ 40        J      CLOOP
+ 45 ENDFIL LDA    =C'EOF'
+ 50        STA    BUFFER
+ 55        LDA    #3
+ 60        STA    LENGTH
+ 65        +JSUB  WRREC
+ 70        J      @RETADR
+ 93        LTORG
+ 95 RETADR RESW   1
+100 LENGTH RESW   1
+105 BUFFER RESB   4096
+106 BUFEND EQU    *
+107 MAXLEN EQU    BUFEND-BUFFER
+110 .
+115 .      SUBROUTINE TO READ RECORD INTO BUFFER
+120 .
+125 RDREC  CLEAR  X
+130        CLEAR  A
+132        CLEAR  S
+133        +LDT   #MAXLEN
+135 RLOOP  TD     INPUT
+140        JEQ    RLOOP
+145        RD     INPUT
+150        COMPR  A, S
+155        JEQ    EXIT
+160        STCH   BUFFER, X
+165        TIXR   T
+170        JLT    RLOOP
+175 EXIT   STX    LENGTH
+180        RSUB
+185 INPUT  BYTE   X'F1'
+195 .
+200 .      SUBROUTINE TO WRITE RECORD FROM BUFFER
+205 .
+210 WRREC  CLEAR  X
+212        LDT    LENGTH
+215 WLOOP  TD     =X'05'
+220        JEQ    WLOOP
+225        LDCH   BUFFER, X
+230        WD     =X'05'
+235        TIXR   T
+240        JLT    WLOOP
+245        RSUB
+255        END    FIRST
